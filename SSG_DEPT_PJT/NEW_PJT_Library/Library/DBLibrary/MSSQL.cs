252f4e80baa079c61696a918dded17d936361db7
ydk831 <50939222+ydk831@users.noreply.github.com>
2022-07-17 17:04:23 +0900

    Add files via upload

---
+++ SSG_DEPT_PJT/NEW_PJT_Library/Library/DBLibrary/MSSQL.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LogLibrary;
+
+
+namespace DBLibrary
+{
+    public enum DB_LOCATION { LOCAL, REMOTE };
+    public class MSSQL : IDisposable
+    {
+        /// <summary>
+        /// Singleton 객체 폐기 Flag
+        /// </summary>
+        private bool m_isDispose = false;
+        /// <summary>
+        /// MSSQL Singleton 객체
+        /// </summary>
+        private static MSSQL m_instance;
+        /// <summary>
+        /// Singleton 객체 접근을 위한 Lock
+        /// </summary>
+        private static object m_instanceLock = new object();
+        /// <summary>
+        /// 점포코드 별 SqlConnectionManger 객체를 갖는 Dictionary (로컬서버)
+        /// </summary>
+        private static ConcurrentDictionary<string, SqlConnectinoManager> m_localSQLDictionary = new ConcurrentDictionary<string, SqlConnectinoManager>();
+        /// <summary>
+        /// 점포코드 별 SqlConnectionManger 객체를 갖는 Dictionary (원격서버)
+        /// </summary>
+        private static ConcurrentDictionary<string, SqlConnectinoManager> m_remoteSQLDictionary = new ConcurrentDictionary<string, SqlConnectinoManager>();
+        /// <summary>
+        /// 외부에서 생성자 접근을 못하게 private로 명시 
+        /// </summary>      
+        private MSSQL() { }
+        ~MSSQL()
+        {
+            if (!m_isDispose)
+            {
+                Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Singleton 객체 폐기 함수
+        /// </summary>
+        public void Dispose()
+        {
+            m_isDispose = true;
+
+            // 메모리 해제
+            foreach (string key in m_localSQLDictionary.Keys)
+            {
+                m_localSQLDictionary[key].Dispose();
+            }
+            m_localSQLDictionary.Clear();
+
+            GC.SuppressFinalize(this); // GC가 이 객체에 대해서 Finalize를 호출하지 않도록 명시적으로 지정
+        }
+
+        /// <summary>
+        /// Singleton 처리를 위한 객체 getter
+        /// </summary>
+        public static MSSQL Instance
+        {
+            get
+            {
+                // lock은 비싼 연산에 속하므로 Double-checked locking을 사용
+                // 자세한 설명 : https://stackoverflow.com/questions/12316406/thread-safe-c-sharp-singleton-pattern
+                if (m_instance == null)
+                {
+                    lock (m_instanceLock)
+                    {
+                        if (m_instance == null)
+                        {
+                            m_instance = new MSSQL();
+                        }
+                    }
+                }
+                return m_instance;
+            }
+        }
+
+        /// <summary>
+        /// MSSQL 객체 초기화
+        /// 서비스에서 점포코드별 연동 할 DB정보를 입력하면 해당 정보를 토대로 SqlonnectinoManager 클래스를 생성하여 m_localSQLDictionary에 저장한다.
+        /// </summary>
+        /// <param name="storeCode">점포코드 4자리</param>
+        /// <param name="dbIp">접속할 DB IP</param>
+        /// <param name="dbName">접속할 Database 명</param>
+        /// <param name="dbUser">접속할 DB 계정명</param>
+        /// <param name="dbPassword">접속할 DB 계정의 비밀번호</param>
+        /// <param name="connectionCnt">DB와 연결을 맺을 세션 수</param>
+        /// <param name="dbConnTimeout">DB연결 TImeout 시간 (ms)</param>
+        /// <returns>초기화 성공여부</returns>
+        public bool Initialize(DB_LOCATION dbLocation, string storeCode, string dbIp, string dbName, string dbUser, string dbPassword, uint connectionCnt, int dbConnTimeout)
+        {
+            try
+            {
+                if (storeCode.Length != 4)
+                {
+                    // 점포코드 정합성 에러
+                    return false;
+                }
+                else if (dbIp == null || IPAddress.TryParse(dbIp, out IPAddress addr) == false)
+                {
+                    // dbIp형식 에러
+                    return false;
+                }
+                else if (connectionCnt <= 0)
+                {
+                    // db연결 세션 수 에러, 1로 초기화
+                    connectionCnt = 1;
+                }
+                else if (dbConnTimeout < 1000)
+                {
+                    // 1초보다 적게 설정했을 경우 1초로 초기화
+                    dbConnTimeout = 1000;
+                }
+
+                // 로컬DB 연결
+                if (dbLocation == DB_LOCATION.LOCAL)
+                {
+                    if (m_localSQLDictionary.TryAdd(storeCode, new SqlConnectinoManager(storeCode, dbIp, dbName, dbUser, dbPassword, connectionCnt, dbConnTimeout)))
+                    {
+                        // 연결로그 작성
+                        LogManager.Instance.Log("success local db", Thread.CurrentThread.ManagedThreadId);
+                        return true;
+                    }
+                    else // 동일 Key가 존재하는 경우 Fail
+                    {
+                        // 에러로그 작성
+                        return false;
+                    }
+                }
+                else // 원격DB 연결
+                {
+                    if (m_remoteSQLDictionary.TryAdd(storeCode, new SqlConnectinoManager(storeCode, dbIp, dbName, dbUser, dbPassword, connectionCnt, dbConnTimeout)))
+                    {
+                        // 연결로그 작성
+                        LogManager.Instance.Log("success remote db", Thread.CurrentThread.ManagedThreadId);
+                        return true;
+                    }
+                    else // 동일 Key가 존재하는 경우 Fail
+                    {
+                        // 에러로그 작성
+                        return false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // exception 에러로그 발생
+                LogManager.Instance.Log($"MSSQL Exception {e.Message}", Thread.CurrentThread.ManagedThreadId);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 점포코드에 매칭되는 SqlConnectinoManager 객체를 가져오는 함수
+        /// GetConnectino(string storeCode)에서만 호출한다.
+        /// </summary>
+        /// <param name="connectionCount">점포코드 4자리</param>
+        /// <returns>해당 점포코드에 매칭되는 SqlConnectionManager 객체</returns>
+        internal SqlConnectinoManager GetSqlConnectinoManager(string storeCode, DB_LOCATION dbLocation = DB_LOCATION.LOCAL)
+        {
+            try
+            {
+                if (dbLocation == DB_LOCATION.LOCAL)
+                {
+                    if (m_localSQLDictionary.TryGetValue(storeCode, out SqlConnectinoManager sqlConnMgr))
+                    {
+                        return sqlConnMgr;
+                    }
+                    else // SqlConnectionManager가 Dictionary에 없는 경우.
+                    {
+                        // 에러로그
+                        return null;
+                    }
+                }
+                else
+                {
+                    if (m_remoteSQLDictionary.TryGetValue(storeCode, out SqlConnectinoManager sqlConnMgr))
+                    {
+                        return sqlConnMgr;
+                    }
+                    else // SqlConnectionManager가 Dictionary에 없는 경우.
+                    {
+                        // 에러로그
+                        return null;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // exception 에러로그                
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 서비스에서 사용하는 실제 DB세션 객체를 얻어오는 함수.
+        /// </summary>
+        /// <param name="storeCode">점포코드 4자리</param>
+        /// <returns>DB세션객체</returns>
+        public SqlConnection GetConnection(string storeCode, DB_LOCATION dbLocation = DB_LOCATION.LOCAL)
+        {
+            if (storeCode.Length != 4)
+            {
+                // 점포코드 정합성 에러
+                return null;
+            }
+
+            SqlConnectinoManager sqlConnMgr = GetSqlConnectinoManager(storeCode, dbLocation);
+            if (sqlConnMgr == null)
+            {
+                // SqlConnectionManager 오류. 없는 점포코드로 조회할 경우 발생할 수 있음. 에러로그 작성
+                return null;
+            }
+
+            // return 되는게 null인것도 사용하는 측에서 체크
+            return sqlConnMgr.GetConnection();
+        }
+
+        /// <summary>
+        /// 서비스에서 사용한 DB세션 객체를 반환하는 함수
+        /// </summary>
+        /// <param name="storeCode">점포코드 4자리</param>
+        /// <param name="dbConnection">사용한 DB세션객체</param>
+        /// <returns>반환 성공여부</returns>
+        public bool ReturnConnection(string storeCode, SqlConnection dbConnection, DB_LOCATION dbLocation = DB_LOCATION.LOCAL)
+        {
+            if (storeCode.Length != 4)
+            {
+                // 점포코드 정합성 에러
+                return false;
+            }
+            SqlConnectinoManager sqlConnMgr = GetSqlConnectinoManager(storeCode, dbLocation);
+            if (sqlConnMgr == null)
+            {
+                // SqlConnectionManager 오류. 없는 점포코드로 조회할 경우 발생할 수 있음. 에러로그 작성
+                return false;
+            }
+
+            sqlConnMgr.ReturnConnection(dbConnection);
+            return true;
+        }
+    }
+}
---
+++ SSG_DEPT_PJT/NEW_PJT_Library/Library/DBLibrary/SqlConnectionManager.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LogLibrary;
+
+namespace DBLibrary
+{
+    /// <summary>
+    /// DB연결 및 관리, 연결된 세션 객체를 보관, 반환, 회수하는 Class
+    /// 점포코드별로 생성된다.
+    /// </summary>
+    internal class SqlConnectinoManager : IDisposable
+    {
+        /// <summary>
+        /// Singleton 객체 폐기 Flag
+        /// </summary>
+        private bool m_isDispose = false;
+        /// <summary>
+        /// 연결되지 않은 SqlConnection 객체를 보관하는 큐
+        /// </summary>
+        internal ConcurrentQueue<SqlConnection> m_sqlDisConnectionQ = new ConcurrentQueue<SqlConnection>();
+        /// <summary>
+        /// 연결된 SqlConnectino 세션 객체를 보관하는 큐
+        /// </summary>
+        internal ConcurrentQueue<SqlConnection> m_sqlConnectionQ = new ConcurrentQueue<SqlConnection>();
+
+        /// <summary>
+        /// 점포코드 4자리
+        /// </summary>
+        internal string StoreCode { get; set; }
+        /// <summary>
+        /// 연결할 DB서버 IP
+        /// </summary>
+        internal string DbIp { get; set; }
+        /// <summary>
+        /// 연결할 DB서버 명칭
+        /// </summary>
+        internal string DbName { get; set; }
+        /// <summary>
+        /// DB연결에 사용 될 계정
+        /// </summary>
+        internal string DbUser { get; set; }
+        /// <summary>
+        /// DB연결에 사용 될 계정 비밀번호
+        /// </summary>
+        internal string DbPassword { get; set; }
+        /// <summary>
+        /// DB연결에 사용 될 DB연결문자열
+        /// </summary>
+        internal string DbConnectString { get; set; }
+        /// <summary>
+        /// DB와 연결 할 세션 수
+        /// </summary>
+        internal uint DbConnectionCnt { get; set; }
+        /// <summary>
+        /// DB 연결 Timeout sec
+        /// </summary>
+        internal int DbConnectionTimeout { get; set; }
+
+        CancellationTokenSource m_CheckDisConnSessionTaskCT = new CancellationTokenSource();
+
+        Task m_CheckDisconnSessionTask;
+
+        /// <summary>
+        /// DBMS 클래스에서 생성자를 호출한다.
+        /// 파라미터로 전달 된 정보를 토대로 DB세션 객체를 생성하고 DisConnectionQ에 저장한다.
+        /// 그리고 CheckDisconnSession()을 Task로 수행하여 연결되지 않은 DB객체들을 연결한다.
+        /// </summary>
+        /// <param name="storeCode">점포코드 4자리</param>
+        /// <param name="dbIp">DB서버 IP</param>
+        /// <param name="dbName">DB서버 명</param>
+        /// <param name="dbUser">DB 접속 계정</param>
+        /// <param name="dbPassword">DB 접속 계정의 비밀번호</param>
+        /// <param name="m_dbConnectionCnt">DB에 연결 할 세션 수</param>
+        /// <param name="dbConnectionTimeout">DB 연결 Timeout 시간</param>
+        internal SqlConnectinoManager(string storeCode, string dbIp, string dbName, string dbUser, string dbPassword, uint m_dbConnectionCnt, int dbConnectionTimeout)
+        {
+            StoreCode = storeCode;
+            DbIp = dbIp;
+            DbName = dbName;
+            DbUser = dbUser;
+            DbPassword = dbPassword;
+            DbConnectionTimeout = dbConnectionTimeout;
+            DbConnectString = $"Server={DbIp}; Database={DbName}; uid={DbUser}; pwd={DbPassword}; Connection Timeout={dbConnectionTimeout}";
+
+            for (int i = 0; i < m_dbConnectionCnt; i++)
+            {
+                m_sqlDisConnectionQ.Enqueue(new SqlConnection(DbConnectString));
+            }
+
+            m_CheckDisconnSessionTask = CheckDisconnSession();
+            LogManager.Instance.Log("SqlConnectionManager Create Success", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        ~SqlConnectinoManager()
+        {
+            if (!m_isDispose)
+            {
+                Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Singleton 객체 폐기 함수
+        /// </summary>
+        public void Dispose()
+        {
+            m_isDispose = true;
+
+            // 메모리 정리
+            m_CheckDisConnSessionTaskCT.Cancel();
+
+            GC.SuppressFinalize(this); // GC가 이 객체에 대해서 Finalize를 호출하지 않도록 명시적으로 지정
+        }
+
+        /// <summary>
+        /// 생성자에서 TASK로 호출되며, 연결되지 않은 세션들을 연결한다.
+        /// CancellationToken을 이용해 연결 Timeout을 지정한다.
+        /// </summary>
+        /// <returns></returns>
+        internal async Task CheckDisconnSession()
+        {
+            LogManager.Instance.Log("SqlConnectinoManager Start CheckDisConnSession", Thread.CurrentThread.ManagedThreadId);
+            while (true)
+            {
+                if (m_CheckDisConnSessionTaskCT.Token.IsCancellationRequested)
+                {
+                    break;
+                }
+                try
+                {
+                    if (m_sqlDisConnectionQ.TryDequeue(out SqlConnection sqlConn))
+                    {
+                        if (sqlConn == null)
+                        {
+                            LogManager.Instance.Log("SqlConnectino is null", Thread.CurrentThread.ManagedThreadId);
+                            sqlConn = new SqlConnection(DbConnectString);
+                            //using (CancellationTokenSource dbOpenTC = new CancellationTokenSource((int)DbConnectionTimeout))
+                            //{
+                            //    //dbOpenTC.CancelAfter((int)DbConnectionTimeout);
+                            //    await sqlConn.OpenAsync(dbOpenTC.Token);
+                            //}
+
+                            sqlConn.Open();
+                            m_sqlConnectionQ.Enqueue(sqlConn);
+                            LogManager.Instance.Log($"SqlConnect Enqueue with null", Thread.CurrentThread.ManagedThreadId);
+                        }
+                        else if (sqlConn.State != System.Data.ConnectionState.Open)
+                        {
+                            LogManager.Instance.Log($"SqlConnectino state is {sqlConn.State}", Thread.CurrentThread.ManagedThreadId);
+                            //sqlConn.ConnectionString = DbConnectString;
+                            await sqlConn.OpenAsync();
+                            LogManager.Instance.Log($"SqlConnect Enqueue with {sqlConn.State}", Thread.CurrentThread.ManagedThreadId);
+                            m_sqlConnectionQ.Enqueue(sqlConn);
+                        }
+                        else // 그 외 상태들에 대해서...
+                        {
+                            // 뭐할까...
+                        }
+                    }
+                    else
+                    {
+                        // Dequeue Error.. or no more exist dequeue session
+                        //LogManager.Instance.Log("CheckDisconnsession can't deque", Thread.CurrentThread.ManagedThreadId);
+                    }
+                }
+                catch (Exception e)
+                {
+                    // 에러 출력
+                    LogManager.Instance.Log($"CheckDisconnsession {e.Message}", Thread.CurrentThread.ManagedThreadId);
+                }
+
+                await Task.Delay(100);
+            }
+            LogManager.Instance.Log($"SqlConnectionManager Ended", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        /// <summary>
+        /// DBMS 싱글톤 객체에서 커넥션을 가져가기 위한 함수
+        /// </summary>
+        /// <returns>DB세션 객체</returns>
+        internal SqlConnection GetConnection()
+        {
+            try
+            {
+                // 연결 큐에서 연결이 맺어진 객체를 꺼낼 때 까지
+                while (m_sqlConnectionQ.TryDequeue(out SqlConnection sqlConn))
+                {
+                    // Open 상태가 아니면 끊고 비연결큐에 넣음 (자동 재접속되게)
+                    if (sqlConn.State != System.Data.ConnectionState.Open)
+                    {
+                        sqlConn.Close();
+                        m_sqlDisConnectionQ.Enqueue(sqlConn);
+                    }
+                    // Open 상태인건 리턴
+                    else
+                    {
+                        return sqlConn;
+                    }
+
+                }
+
+                // 연결큐에 연결된게 하나도 없다면 에러로그 남기고 null 리턴
+                return null;
+
+            }
+            catch
+            {
+                // 익셉션
+                return null;
+            }
+        }
+
+        internal void ReturnConnection(SqlConnection sqlConn)
+        {
+            m_sqlConnectionQ.Enqueue(sqlConn);
+        }
+
+    }
+}
